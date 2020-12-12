1f5616c77a32d436004e62014d324d54c54bfcc1
Keivan Mojmali <mojmalik@gmail.com>
2020-12-11 17:32:12 -0800

    issue 3 resolved

--- main.js
+++ main.js
@@ -5,59 +5,53 @@ var intervalId = null;
 var $carContainerStyle = $carContainer.style;
 var topElf = 0;
 var leftShake = 0;
-var emptyInside = '';
+var emptyString = '';
 
 
 function movedirection(key) {
   if (key === 'ArrowUp') {
     $car.className = 'car-size rotate-up';
-    if(topElf === 0) {
-      return;
-    } else {
+    if(topElf !== 0) {
       topElf -= 1;
-      emptyInside = 'top:'+topElf + 'px;left:'+leftShake+'px;position:absolute';
-      $carContainer.setAttribute('style',emptyInside);
+      $carContainer.style.left = leftShake.toString() + 'px';
+      $carContainer.style.top = topElf.toString() + 'px';
       clearInterval(intervalId);
+      console.log(topElf.toString());
     }
   }
   if (key === 'ArrowLeft') {
     $car.className = 'car-size rotate-left';
-    if (leftShake === 0) {
-      return;
-    } else {
+    if (leftShake !== 0) {
       leftShake -= 1;
-      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
-      $carContainer.setAttribute('style', emptyInside);
+      $carContainer.style.left = leftShake.toString() + 'px';
+      $carContainer.style.top = topElf.toString() + 'px';
       clearInterval(intervalId);
     }
   }
   if (key === 'ArrowRight') {
     $car.className = 'car-size roate-right';
-    if (leftShake === 1000) {
-      return;
-    } else {
+    if (leftShake !== 1000) {
       leftShake += 1;
-      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
-      $carContainer.setAttribute('style', emptyInside);
+      $carContainer.style.left = leftShake.toString() + 'px';
+      $carContainer.style.top = topElf.toString() + 'px';
       clearInterval(intervalId);
+
     }
   }
   if (key === 'ArrowDown') {
     $car.className = 'car-size rotate-down';
-    if (topElf === 750) {
-      return;
-    } else {
+    if (topElf !== 750) {
       topElf += 1;
-      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
-      $carContainer.setAttribute('style', emptyInside);
+      $carContainer.style.left = leftShake.toString() + 'px';
+      $carContainer.style.top = topElf.toString() + 'px';
+      clearInterval(intervalId);
 
     }
   }
   if(key === 'Space') {
-    topElf += 1;
     leftShake += 1;
-    emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
-    $carContainer.setAttribute('style', emptyInside);
+    $carContainer.style.left = leftShake.toString()+'px';
+    $carContainer.style.top = topElf.toString()+'px';
   }
 }
 
