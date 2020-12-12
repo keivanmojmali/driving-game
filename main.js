3b863a15fc230ac10d926c8d4f6f11f384cf9854
Keivan Mojmali <mojmalik@gmail.com>
2020-12-11 16:22:00 -0800

    issue 3 resolved

--- main.js
+++ main.js
@@ -1,22 +1,74 @@
 var $carContainer = document.querySelector('#car');
 var $car = document.querySelector('.car-size');
 var currentDirection;
+var intervalId = null;
+var $carContainerStyle = $carContainer.style;
+var topElf = 0;
+var leftShake = 0;
+var emptyInside = '';
+
 
 function movedirection(key) {
   if (key === 'ArrowUp') {
     $car.className = 'car-size rotate-up';
+    if(topElf === 0) {
+      return;
+    } else {
+      topElf -= 1;
+      emptyInside = 'top:'+topElf + 'px;left:'+leftShake+'px;position:absolute';
+      $carContainer.setAttribute('style',emptyInside);
+      clearInterval(intervalId);
+    }
   }
   if (key === 'ArrowLeft') {
     $car.className = 'car-size rotate-left';
+    if (leftShake === 0) {
+      return;
+    } else {
+      leftShake -= 1;
+      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
+      $carContainer.setAttribute('style', emptyInside);
+      clearInterval(intervalId);
+    }
   }
   if (key === 'ArrowRight') {
     $car.className = 'car-size roate-right';
+    if (leftShake === 1000) {
+      return;
+    } else {
+      leftShake += 1;
+      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
+      $carContainer.setAttribute('style', emptyInside);
+      clearInterval(intervalId);
+    }
   }
   if (key === 'ArrowDown') {
     $car.className = 'car-size rotate-down';
+    if (topElf === 750) {
+      return;
+    } else {
+      topElf += 1;
+      emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
+      $carContainer.setAttribute('style', emptyInside);
+
+    }
+  }
+  if(key === 'Space') {
+    topElf += 1;
+    leftShake += 1;
+    emptyInside = 'top:' + topElf + 'px;left:' + leftShake + 'px;position:absolute';
+    $carContainer.setAttribute('style', emptyInside);
   }
 }
 
+function startCar() {
+movedirection('Space');
+}
+
+
 document.addEventListener('keydown', function (e) {
   movedirection(e.key)
+  if(e.code === 'Space') {
+    intervalId = setInterval(startCar,16);
+  }
 });
