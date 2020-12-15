b35dbe855dc26dc081f4b7d7fed7e94dd3e7f50f
Keivan Mojmali <mojmalik@gmail.com>
2020-12-14 17:16:19 -0800

    working on it still

--- main.js
+++ main.js
@@ -3,24 +3,43 @@ var $car = document.querySelector('.car-size');
 var intervalId = null;
 var topElf = 0;
 var leftShake = 0;
-var movementCheck = true;
+var movementCheck = false;
+var direction = 'Arrowright';
 
 function rotate(key) {
   if (key === 'ArrowUp') {
+    topElf--;
     $car.className = 'car-size rotate-up';
+    direction = 'ArrowUp';
+    $carContainer.style.left = leftShake + 'px';
+    $carContainer.style.top = topElf + 'px';
+  }
+  if (key === 'ArrowRight') {
+    leftShake++;
+    $car.className = 'car-size rotate-right';
+    direction = 'ArrowRight';
+    $carContainer.style.left = leftShake + 'px';
+    $carContainer.style.top = topElf + 'px';
   }
   if (key === 'ArrowLeft') {
+    leftShake--;
     $car.className = 'car-size rotate-left';
+    direction = 'ArrowLeft';
+    $carContainer.style.left = leftShake + 'px';
+    $carContainer.style.top = topElf + 'px';
   }
   if (key === 'ArrowDown') {
+    topElf--;
     $car.className = 'car-size rotate-down';
+    direction = 'ArrowDown';
+    $carContainer.style.left = leftShake + 'px';
+    $carContainer.style.top = topElf + 'px';
   }
+
 }
 
-function move(key) {
-  leftShake++;
-  $carContainer.style.left = leftShake + 'px';
-  $carContainer.style.top = topElf + 'px';
+function move() {
+  rotate(direction);
 }
 
 document.addEventListener('keydown', function (e) {
